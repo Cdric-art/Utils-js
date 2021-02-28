95fa33b84762c1d6001b1af22b0950eae18dc8fd
Cdric-art <cdric.art@gmail.com>
2021-02-28 13:04:15 +0100

    Add apiFetch

---
+++ api.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * @param {string} endpoint
+ * @param {object} options
+ * @returns {Promise<void>}
+ */
+
+export async function apiFetch(endpoint, options = {}) {
+  options = {
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+    },
+    ...options,
+  }
+  if (options.body !== null && typeof options.body === 'object' && !(options.body instanceof FormData)) {
+    options.body = JSON.stringify(options.body)
+    options.headers['Content-Type'] = 'application/json'
+  }
+  const response = await fetch(`http://127.0.0.1:3333${endpoint}`, options);
+  if (response.status === 204) {
+    return null;
+  }
+  const responseData = await response.json();
+  if (response.ok) {
+    return responseData;
+  } else {
+    if (responseData.errors) {
+      throw new ApiErrors(responseData.errors)
+    }
+  }
+}
+
+
+export class ApiErrors {
+  constructor(errors) {
+    this.errors = errors
+  }
+
+  get errorsPerField() {
+    return this.errors.reduce((acc, error) => {
+      acc[error.field] = error.message
+      return acc
+    }, {})
+  }
+
+}
